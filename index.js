d1d6364521d8105ea5878ddba6226ba0c9ccb5c5
Lokogame <96664635+Lokogame@users.noreply.github.com>
2021-12-25 20:44:13 +0100

    Uppload files

---
+++ index.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js")
+const client = new Discord.Client(
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
+)
+
+client.login("OTI0MjM0MjIzMzI0MzgxMjI0.YcbmMg.cWi935XT_6zZvJfNppT5-3QitJg")
+
+client.on("ready", () => {
+console.log("Bot ONLINE")
+
+
+
+})
+
+client.on("messageCreate", (message) => {
+  if(message.content == "!youtube"){
+    message.channel.send("Questo è il mio canale: https://www.youtube.com/channel/UCQtVVCV47nxraIi1xP9jXWg")
+  }
+
+  if(message.content == "!telegram"){
+      var embed = new Discord.MessageEmbed()
+      .setTitle("Gruppo Telegram")
+      .setDescription("Il Gruppo Telegram serve per aggiornati sulle live, server discord, canale youtube e molto altro. Il link Telegram è il seguente: https://t.me/Player_community")
+      .setThumbnail("https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Telegram_alternative_logo.svg/1200px-Telegram_alternative_logo.svg.png")
+
+
+      message.channel.send({embeds: [embed]})
+  }
+
+
+
+})
